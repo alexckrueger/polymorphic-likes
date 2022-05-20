3c34420633f9889b6c40e75336be221ce510a825
Alex Krueger <alexckrueger@gmail.com>
2022-05-20 17:05:12 -0500

    adds user_id to likes and well seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_205554) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_215654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_205554) do
     t.bigint "likeable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  {name: "Alex"},
+  {name: "Bear"},
+  {name: "Tara"}
+])
+
+Post.create!([
+  {user_id: 1, title: "How to handle your small puppy", body: "Give them a lot of pets and treats."},
+  {user_id: 3, title: "The best treats to give your dog", body: "Greenies"}
+])
+
+Comment.create!([
+  {user_id: 2, post_id: 1, body: "Yes! Give them all the pets and all the treats!"},
+  {user_id: 2, post_id: 2, body: "GREENIES"}
+])
+
+Like.create!([
+  {:likeable => Post.first, user_id: 2},
+  {:likeable => Post.second, user_id: 2},
+  {:likeable => Comment.first, user_id: 1},
+  {:likeable => Comment.second, user_id: 3}
+])
