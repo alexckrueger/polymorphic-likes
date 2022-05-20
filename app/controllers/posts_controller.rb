8ae31d86befdd1778a8bf19ef5f6636410ea2859
Alex Krueger <alexckrueger@gmail.com>
2022-05-20 17:14:45 -0500

    adds jb gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
 
   def index
-    posts = Post.all
-    render json: posts
+    @posts = Post.all
+    render :index
   end
 
   def show
-    post = Post.find(params[:id])
-    render json: post
+    @post = Post.find(params[:id])
+    render :show
   end
 
 end
